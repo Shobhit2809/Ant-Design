82d9101adf9bc2779aad8fd4321891c8c617cfee
Shobhit Agrawal <shobhitagrawal2809@gmail.com>
2023-02-13 22:16:16 +0530

    selection of rows and custom filtering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/reset.css';
-import {Table} from 'antd'
+import {Table, Tag} from 'antd'
 import { useEffect, useState } from 'react';
 import {PoweroffOutlined} from '@ant-design/icons'
 
@@ -9,67 +9,110 @@ import {PoweroffOutlined} from '@ant-design/icons'
 
 function App() {
   
-  const[loading,setLoading]  = useState(false)
-  const [dataSource, setdataSource] = useState([])
-  const [page,setPage] = useState(1)
-  const [pageSize,setPageSize] = useState(10)
-
-  useEffect(()=>{
-    setLoading(true)
-    fetch("https://jsonplaceholder.typicode.com/todos")
-    .then(response=>response.json())
-    .then(data=>setdataSource(data))
-    .catch(err=>{console.log(err);})
-    .finally(setLoading(false))
-  },[])
+  const[alreadySelectedRows ,setalreadySelectedRows] = useState(['1','2'])
 
   const columns= [
   {
-    key:"1",
-    title:'ID',
+    title:'Student ID',
     dataIndex:'id'
   },
   {
-    key:"2",
-    title:'User ID',
-    dataIndex:'userId',
-    sorter:(record1,record2)=>{
-      return record1.userId>record2.userId
-    }
+    title:'Student Name',
+    dataIndex:'name'
   },
   {
-    key:"3",
-    title:'Status',
-    dataIndex:'completed',
-    render:(completed)=>{
-      return <p>{completed?'Complete':'In Progress'}</p>
-    },
-    filters:[
-      {text:'Complete',value:true},
-      {text:'In Progress',value:false}
-    ],
-    onFilter:(value, record)=>{
-      return record.completed === value
+    title:'Student Grade',
+    dataIndex:'grade',
+    render:(tag)=>{
+      const color = tag.includes('A')?'Green':tag.includes('B')?"blue":"red"
+      return <Tag color={color} key={tag}>{tag}</Tag>
     }
   },
+  ]
+
+  const dataSource = [
+    {
+      key:'1',
+      id:1,
+      name: 'Student Name 1',
+      grade:'A+'
+    },
+    {
+      key:'2',
+      id:2,
+      name: 'Student Name 2',
+      grade:'A'
+    },
+    {
+      key:'3',
+      id:3,
+      name: 'Student Name 3',
+      grade:'B'
+    },
+    {
+      key:'4',
+      id:4,
+      name: 'Student Name 4',
+      grade:'C'
+    },
+    {
+      key:'5',
+      id:5,
+      name: 'Student Name 5',
+      grade:'A'
+    },
+    
   ]
 
   return (
     <div className="App">
       <header className="App-header">
-        <Table loading={loading} dataSource={dataSource} 
-        columns={columns} pagination={{
-          current:page,
-          // rows per page
-          pageSize:pageSize,
-          onChange:(page,pageSize)=>{
-            setPage(page)
-            setPageSize(pageSize)
-          }
-
-        }}>
-
-        </Table>
+        <Table columns={columns} dataSource={dataSource} rowSelection={{
+          type:'checkbox',
+          selectedRowKeys:alreadySelectedRows,
+          onChange:(keys)=>{
+            setalreadySelectedRows(keys)
+          },  
+          onSelect:(record)=>{
+            console.log({record});
+          },
+          getCheckboxProps:(record)=>({
+            // if its a c grade disable selection
+            disabled:record.grade === 'C'
+          }),
+          // hideSelectAll:true,
+          selections:[
+            Table.SELECTION_NONE,
+            Table.SELECTION_ALL,
+            // select all those which are currently not selected
+            Table.SELECTION_INVERT,
+            {
+              key:'even',
+              text:'Select Even Rows',
+              // gives all keys
+              onSelect:(allKeys)=>{
+                const selectedKeys = allKeys.filter(key=>{
+                  // even keys
+                  return key %2 ==0
+                })
+                setalreadySelectedRows(selectedKeys)
+              }
+            },
+            {
+              key:'excellent',
+              text:'Select Students with Excellent grades',
+              onSelect:(allKeys)=>{
+                const selectedKeys = allKeys.filter(key=>{
+                    const isExcellent = dataSource.find(student =>{
+                    return student.key==key && student.grade.includes('A')
+                  })
+                  return isExcellent
+                })
+                setalreadySelectedRows(selectedKeys)
+              }
+            }
+          ]
+        }}></Table>
       </header>
     </div>
   );
