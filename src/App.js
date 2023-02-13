ed4cda70cbb947794c9b3ac3846165d32425578a
Shobhit Agrawal <shobhitagrawal2809@gmail.com>
2023-02-13 17:16:29 +0530

    Select Component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/reset.css';
-import {Button} from 'antd'
+import {Select} from 'antd'
 import { useState } from 'react';
 import {PoweroffOutlined} from '@ant-design/icons'
 
-function App() {
-  const dabaMera = (e) =>{
-    console.log('daba diya');
-  }
-  const[loading,setLoading] = useState(true)
-  setTimeout(()=>{
-    setLoading(false)
-  },2000)
 
+
+function App() {
+  const fruits = ['Banana','Mango','Orange','Curry']
 
   return (
     <div className="App">
       <header className="App-header">
-        <Button type='primary' block onClick={dabaMera} loading={loading}
-        className='my_button'
-        style={{backgroundColor:'orange',color:'red'}}
-        icon={<PoweroffOutlined/>}>
-          Lauda Mera
-        </Button>
+        <p>Which is your favourite food?</p>
+        {/* You can also search in it */}
+        <Select mode='multiple' allowClear maxTagCount={2} placeholder='Select fruit' style={{width:'50%'}}>
+          {fruits.map((fruit,index)=>{
+            return <Select.Option key={index} value={fruit}></Select.Option>
+          })}
+        </Select>
       </header>
     </div>
   );
