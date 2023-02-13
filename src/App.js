b3299e842a58641f6fde3c5709d91f82730fb8f5
Shobhit Agrawal <shobhitagrawal2809@gmail.com>
2023-02-13 15:54:37 +0530

    button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/reset.css';
+import {Button} from 'antd'
+import { useState } from 'react';
+import {PoweroffOutlined} from '@ant-design/icons'
 
 function App() {
+  const dabaMera = (e) =>{
+    console.log('daba diya');
+  }
+  const[loading,setLoading] = useState(true)
+  setTimeout(()=>{
+    setLoading(false)
+  },2000)
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type='primary' block onClick={dabaMera} loading={loading}
+        className='my_button'
+        style={{backgroundColor:'orange',color:'red'}}
+        icon={<PoweroffOutlined/>}>
+          Lauda Mera
+        </Button>
       </header>
     </div>
   );
